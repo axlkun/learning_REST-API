b65a9f9d1a2b9cdc6923f6a8bbfb80b61826ba51
axlkun <axelcruz.dev@gmail.com>
2023-01-21 14:46:54 -0600

    adding methods

--- usuarios-backend/api/usuarios.php
+++ usuarios-backend/api/usuarios.php
@@ -4,32 +4,37 @@
 
 //echo 'Informacion: ' . file_get_contents('php://input');
 header("Content-Type: application/json");
+include_once('../classes/Usuario.php');
 switch($_SERVER['REQUEST_METHOD']){
 
     case 'POST':
         //transforma el formato json en un arreglo asociativo
-        $_POST = json_decode(file_get_contents('php://input'),true); 
+        $_POST = json_decode(file_get_contents('php://input'),true);
+        $usuario = new Usuario($_POST['nombre'],$_POST['apellido'],$_POST['fechaNacimiento'],$_POST['genero']);
+        $usuario->guardarUsuario(); 
         $resultado["mensaje"] = "Guardar el usuario " . json_encode($_POST);
         echo json_encode($resultado);
         break;
     
     case 'GET':
         if(isset($_GET['id'])){
-            $resultado["mensaje"] = "Retornar usuario con id: " . $_GET['id'];
-            echo json_encode($resultado);
+            Usuario::obtenerUsuario($_GET['id']);
         }else{
-            $resultado["mensaje"] = "Retornar todos los usuarios";
-            echo json_encode($resultado);
+            Usuario::obtenerUsuarios();
         }
         break;
 
     case 'PUT':
         $_PUT = json_decode(file_get_contents('php://input'),true); 
+        $usuario = new Usuario($_PUT['nombre'],$_PUT['apellido'],$_PUT['fechaNacimiento'],$_PUT['genero']);
+        $usuario->actualizarUsuario($_GET['id']);
         $resultado["mensaje"] = "Actualizar el usuario con id: " . $_GET['id'] . ", con la siguiente informacion: " . json_encode($_PUT);
         echo json_encode($resultado);
         break;
     
     case 'DELETE':
+
+        Usuario::eliminarUsuario($_GET['id']);
         $resultado['mensaje'] = "Eliminar usuario con el id: " . $_GET['id'];
         echo json_encode($resultado);
         break; 
--- usuarios-backend/classes/Usuario.php
+++ usuarios-backend/classes/Usuario.php
@@ -64,16 +64,55 @@ class Usuario{
     }
 
     public function guardarUsuario(){
+        $contenido = file_get_contents("../data/usuarios.json");
+        $usuarios = json_decode($contenido,true);
+        $usuarios[] = array(
+            "usuario" => $this->nombre,
+            "apellido" => $this->apellido,
+            "fechaNacimiento" => $this->fechaNacimiento,
+            "genero" => $this->genero
+        );
+        $archivo = fopen("../data/usuarios.json","w");
+        fwrite($archivo,json_encode($usuarios));
+        fclose($archivo);
+    }
+    public static function obtenerUsuarios(){
+        $contenido = file_get_contents("../data/usuarios.json");
+        echo $contenido;
+    }
+
+    public static function obtenerUsuario($indice){
+        $contenido = file_get_contents("../data/usuarios.json"); //cadena json
+        $usuarios = json_decode($contenido,true); //convierte el json en un arreglo asociativo
+        $usuarioEncontrado = json_encode($usuarios[$indice]); //el resultado se vuelve a convertir en formato json
+        echo $usuarioEncontrado;
+    }
+    public function actualizarUsuario($indice){
+        $contenido = file_get_contents("../data/usuarios.json"); //cadena json
+        $usuarios = json_decode($contenido,true); //convierte el json en un arreglo asociativo
         
+        $usuario = array(
+            'nombre'=> $this->nombre,
+            'apellido'=> $this->apellido,
+            'fechaNacimiento'=> $this->fechaNacimiento,
+            'genero'=> $this->genero
+        );
+        //se sustituye el usuario del indice mandado por el creado en el array
+        $usuarios[$indice] = $usuario; 
+        //hasta el momento todo esta en memoria
+        $archivo = fopen('../data/usuarios.json',"w");
+        fwrite($archivo,json_encode($usuarios));
+        fclose($archivo);
     }
-    public function obtenerUsuario(){
 
-    }
-    public function actualizarUsuario(){
-
-    }
-    public function eliminarUsuario(){
-
+    public static function eliminarUsuario($indice){
+        $contenido = file_get_contents("../data/usuarios.json"); //cadena json
+        $usuarios = json_decode($contenido,true); //convierte el json en un arreglo asociativo
+        array_splice($usuarios,$indice,1); //arreglo, indice y cantidad de elementos a eliminar
+        //solo se realizo en memoria por lo que se debe volver a escribir
+        $archivo = fopen('../data/usuarios.json',"w"); //abre el archivo json
+        fwrite($archivo,json_encode($usuarios)); //sustituye el antiguo json por el nuevo
+        fclose($archivo);
     }
     
 }
